0fa3ff8c0b520b729dc955c44814457b15a36e25
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-08 15:27:17 +0200

    17

--- gruntfile.js
+++ gruntfile.js
@@ -14,28 +14,37 @@ module.exports = function(grunt) {
           },
           css: {
             src: ['app/css/src/*.css'],
-            dest: 'dist/css/style.min.css'
+            dest: 'dist/css/style.min.css',
+            src: ['app/css/normalize.css'],
+            dest: 'dist/css/normalize.css'
           },
           html: {
             src: ['app/*.html'],
             dest: 'dist/index.html'
           },
         },
-        uglify: {
+      uglify: {
           js: {
             src:['dist/js/script.min.js'],
             dest: 'dist/js/script.min.js'
           },
-          css:{
-            src: ['dist/css/style.min.css'],
-            dest: 'dist/css/style.min.css'
-          },
         },
+        cssmin: {
+            target: {
+              files: [{
+                expand: true,
+                cwd: 'dist/css',
+                src: ['*.css',],
+                dest: 'dist/css',
+              }]
+            },
+          },
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
